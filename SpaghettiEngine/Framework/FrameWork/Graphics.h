7f148b7da68edfd223ae3253ca0e5018def7b8f1
EvraPrime <62198187+EvraPrime@users.noreply.github.com>
2021-03-29 19:52:30 +0700

    commit

--- SpaghettiEngine/Framework/FrameWork/Graphics.h
+++ SpaghettiEngine/Framework/FrameWork/Graphics.h
@@ -1,6 +1,20 @@
 #pragma once
+#include <d3d9.h>
+
+#pragma comment (lib, "d3d9.lib")
+
+struct CUSTOMVERTEX { FLOAT X, Y, Z, RHW; DWORD COLOR; };
+#define CUSTOMFVF (D3DFVF_XYZRHW | D3DFVF_DIFFUSE)
 
 class Graphics
 {
-
+public:
+    Graphics(HWND hWnd);
+    ~Graphics();
+    void RenderFrame();
+    void InitGraphics();
+protected:
+    LPDIRECT3D9 d3d;    // the pointer to our Direct3D interface
+    LPDIRECT3DDEVICE9 d3ddev;    // the pointer to the device class
+    LPDIRECT3DVERTEXBUFFER9 v_buffer = NULL;
 };
