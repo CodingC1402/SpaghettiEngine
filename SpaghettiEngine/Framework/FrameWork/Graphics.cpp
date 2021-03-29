63f8e9c123e0fc8bfedcec4e68209fc4ca85e783
EvraPrime <62198187+EvraPrime@users.noreply.github.com>
2021-03-29 19:52:43 +0700

    commit

--- SpaghettiEngine/Framework/FrameWork/Graphics.cpp
+++ SpaghettiEngine/Framework/FrameWork/Graphics.cpp
@@ -1,1 +1,78 @@
 #include "Graphics.h"
+
+Graphics::Graphics(HWND hWnd)
+{
+    d3d = Direct3DCreate9(D3D_SDK_VERSION);
+
+    D3DPRESENT_PARAMETERS d3dpp;
+
+    ZeroMemory(&d3dpp, sizeof(d3dpp));
+    d3dpp.Windowed = TRUE;
+    d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+    d3dpp.hDeviceWindow = hWnd;
+    d3dpp.BackBufferFormat = D3DFMT_X8R8G8B8;
+    d3dpp.BackBufferWidth = SCREEN_WIDTH;
+    d3dpp.BackBufferHeight = SCREEN_HEIGHT;
+
+    // create a device class using this information and the info from the d3dpp stuct
+    d3d->CreateDevice(D3DADAPTER_DEFAULT,
+        D3DDEVTYPE_HAL,
+        hWnd,
+        D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+        &d3dpp,
+        &d3ddev);
+
+    InitGraphics();    // call the function to initialize the triangle
+}
+
+Graphics::~Graphics()
+{
+    v_buffer->Release();    // close and release the vertex buffer
+    d3ddev->Release();    // close and release the 3D device
+    d3d->Release();    // close and release Direct3D
+}
+
+void Graphics::RenderFrame()
+{
+    d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
+
+    d3ddev->BeginScene();
+
+    // select which vertex format we are using
+    d3ddev->SetFVF(CUSTOMFVF);
+
+    // select the vertex buffer to display
+    d3ddev->SetStreamSource(0, v_buffer, 0, sizeof(CUSTOMVERTEX));
+
+    // copy the vertex buffer to the back buffer
+    d3ddev->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
+
+    d3ddev->EndScene();
+
+    d3ddev->Present(NULL, NULL, NULL, NULL);
+}
+
+void Graphics::InitGraphics()
+{        // create the vertices using the CUSTOMVERTEX struct
+    CUSTOMVERTEX vertices[] =
+    {
+        { 400.0f, 62.5f, 0.5f, 1.0f, D3DCOLOR_XRGB(0, 0, 255), },
+        { 650.0f, 500.0f, 0.5f, 1.0f, D3DCOLOR_XRGB(0, 255, 0), },
+        { 150.0f, 500.0f, 0.5f, 1.0f, D3DCOLOR_XRGB(255, 0, 0), },
+    };
+
+    // create a vertex buffer interface called v_buffer
+    d3ddev->CreateVertexBuffer(3 * sizeof(CUSTOMVERTEX),
+        0,
+        CUSTOMFVF,
+        D3DPOOL_MANAGED,
+        &v_buffer,
+        NULL);
+
+    VOID* pVoid;    // a void pointer
+
+    // lock v_buffer and load the vertices into it
+    v_buffer->Lock(0, 0, (void**)&pVoid, 0);
+    memcpy(pVoid, vertices, sizeof(vertices));
+    v_buffer->Unlock();
+}
