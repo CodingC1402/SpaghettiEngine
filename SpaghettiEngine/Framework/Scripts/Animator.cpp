a48ecca51d91ac7bb6318e72b0c700e21752ab5c
Nguyen Pham <78258269+CornyCodingCorn@users.noreply.github.com>
2021-04-29 08:21:27 +0700

    Update animation script

--- SpaghettiEngine/Framework/Scripts/Animator.cpp
+++ SpaghettiEngine/Framework/Scripts/Animator.cpp
@@ -1,8 +1,8 @@
 #include "Animator.h"
 #include "GameTimer.h"
+#include "Setting.h"
 
 REGISTER_FINISH(Animator);
-SAnimation* Animator::test;
 
 Animator::Animator()
 {
@@ -13,25 +13,40 @@ void Animator::Update()
 {
 	time += GameTimer::GetDeltaTime();
 	_ani->Advance(&frame, &time);
-	sprite = _ani->GetSpriteOfFrame(&frame);
-	SpriteRenderer::Update();
+}
+
+void Animator::Draw(SpriteHandler handler, PCamera camera)
+{
+	_sprite = _ani->GetSpriteOfFrame(&frame);
+	RECT srcRect = _sprite->GetSourceRect();
+	Vector3 center = _sprite->GetCenter();
+	Matrix transform = camera->GetMatrix(GetWorldMatrix());
+	if (Setting::IsWorldPointPixelPerfect())
+	{
+		transform._41 = std::round(transform._41);
+		transform._42 = std::round(transform._42);
+	}
+
+	handler->SetTransform(&transform);
+	handler->Draw(
+		_sprite->GetSource()->GetImage(),
+		&srcRect,
+		&center,
+		nullptr,
+		WHITE
+	);
 }
 
 void Animator::Load(nlohmann::json& inputObject)
 {
+	ScriptBase::Load(inputObject);
 	try
 	{
 		_ani = Animation::GetAnimation(inputObject["AnimationPath"].get<std::string>());
-		test = &_ani;
 		frame = 0;
 	}
 	catch(const std::exception& e)
 	{
 		throw SCRIPT_FORMAT_EXCEPT(this, "");
 	}
-}
-
-void Animator::Unload()
-{
-	SpriteRenderer::Unload();
 }
--- SpaghettiEngine/Framework/Scripts/Animator.h
+++ SpaghettiEngine/Framework/Scripts/Animator.h
@@ -1,17 +1,17 @@
 #pragma once
-#include "SpriteRenderer.h"
+#include "Render2DScriptBase.h"
 #include "Animation.h"
 
-class Animator : public SpriteRenderer
+class Animator : public Render2DScriptBase
 {
 public:
 	Animator();
 	virtual void Update() override;
-	void Load(nlohmann::json& inputObject) override;
-	virtual void Unload() override;
+	virtual void Load(nlohmann::json& inputObject) override;
+	virtual void Draw(SpriteHandler handler, PCamera camera) override;
 protected:
 	SAnimation _ani;
-	static SAnimation* test;
+	SSprite _sprite;
 	UINT frame = 0;
 	double time = 0;
 private:
