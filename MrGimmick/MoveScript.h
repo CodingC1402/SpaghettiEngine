bcccdf36e940c79fa2201370168556610edc4633
Evra <19520488@gm.uit.edu.vn>
2021-07-13 00:36:11 +0700

    Revert "remove"
    This reverts commit 9773d6ec82294eda2b2431015b70db92b93fe176.

--- MrGimmick/MoveScript.h
+++ MrGimmick/MoveScript.h
@@ -5,6 +5,7 @@
 #include "InputAll.h"
 #include "RigidBody2D.h"
 #include "Animator.h"
+#include "SoundSource.h"
 
 typedef std::shared_ptr<InputAll> SInputAll;
 
@@ -39,6 +40,7 @@ protected:
 	BoolField _isRunningField;
 	BoolField _isGroundedField;
 
+	SoundSource* _audio = nullptr;
 	RigidBody2D* _rigidBody = nullptr;
 	Animator* _animator = nullptr;
 
